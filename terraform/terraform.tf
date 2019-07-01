009c8b2f76f2bf52ff331043eaf1a2012924998f
Felddy <markf@geekpad.com>
2019-07-01 10:22:58 -0400

    Fix terraform state key. Make it match the project name.  This will also get it picked up by the skeleton tool sed fu.

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -4,6 +4,6 @@ terraform {
     bucket         = "playground-terraform-state-storage"
     dynamodb_table = "terraform-state-lock"
     region         = "us-east-1"
-    key            = "ansible-role-dev-ssh-access/terraform.tfstate"
+    key            = "skeleton-packer/terraform.tfstate"
   }
 }
