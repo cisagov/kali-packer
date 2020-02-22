31e3c0a4bf4fe575055667980d50c9f824a0488a
Dave Redmin <david.redmin@beta.dhs.gov>
2020-02-22 15:07:37 -0500

    Switch terraform backend from playground to COOL

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "s3" {
     encrypt        = true
-    bucket         = "playground-terraform-state-storage"
+    bucket         = "cisa-cool-terraform-state"
     dynamodb_table = "terraform-state-lock"
+    profile        = "cool-terraform-backend"
     region         = "us-east-1"
     key            = "skeleton-packer-cool/terraform.tfstate"
   }
