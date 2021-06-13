e6a11218fb24606330e54e04b9f745fcaf648bd0
David Redmin <david.redmin@trio.dhs.gov>
2021-06-13 15:28:58 -0400

    Require AWS provider version to support default tags
    Version 3.38.0 of the Terraform AWS provider is the first version to 
    support default tags.
    https://www.hashicorp.com/blog/default-tags-in-the-terraform-aws-provider

--- terraform-build-user/versions.tf
+++ terraform-build-user/versions.tf
@@ -6,6 +6,9 @@ terraform {
   # major version currently being used.  This practice will help us
   # avoid unwelcome surprises.
   required_providers {
-    aws = "~> 3.0"
+    # Version 3.38.0 of the Terraform AWS provider is the first
+    # version to support default tags.
+    # https://www.hashicorp.com/blog/default-tags-in-the-terraform-aws-provider
+    aws = "~> 3.38"
   }
 }
