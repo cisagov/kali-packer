8763b49d743be4c408709592bbb1410379b169fd
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2023-04-19 09:51:44 -0400

    Use ED25519 instead of RSA ssh keys
    Guacamole supports non-RSA ssh keys as of 1.5.0.

--- terraform-build-user/main.tf
+++ terraform-build-user/main.tf
@@ -16,8 +16,8 @@ module "iam_user" {
     "/gitlab/personal_authorization_token",
     "/neo4j/password",
     "/vnc/password",
-    "/vnc/ssh/rsa_private_key",
-    "/vnc/ssh/rsa_public_key",
+    "/vnc/ssh/ed25519_private_key",
+    "/vnc/ssh/ed25519_public_key",
     "/vnc/username",
   ]
   user_name = "build-kali-packer"
