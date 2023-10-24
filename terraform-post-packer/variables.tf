96655af4959ead9c1cf29518e8c1a40a55283ac7
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2023-10-24 15:51:11 -0400

    Ensure all variable attributes are sorted alphabetically
    This mainly brings the Terraform configuration into alignment with the
    Packer configuration.

--- terraform-post-packer/variables.tf
+++ terraform-post-packer/variables.tf
@@ -5,13 +5,13 @@
 # ------------------------------------------------------------------------------
 
 variable "extraorg_account_ids" {
-  type        = list(string)
-  description = "A list of AWS account IDs corresponding to \"extra\" accounts with which you want to share this AMI (e.g. [\"123456789012\"]).  Normally this variable is used to share an AMI with accounts that are not a member of the same AWS Organization as the account that owns the AMI."
   default     = []
+  description = "A list of AWS account IDs corresponding to \"extra\" accounts with which you want to share this AMI (e.g. [\"123456789012\"]).  Normally this variable is used to share an AMI with accounts that are not a member of the same AWS Organization as the account that owns the AMI."
+  type        = list(string)
 }
 
 variable "recent_ami_count" {
-  type        = number
-  description = "The number of most-recent AMIs for which to grant launch permission (e.g. \"3\").  If this variable is set to three, for example, then accounts will be granted permission to launch the three most recent AMIs (or all most recent AMIs, if there are only one or two of them in existence)."
   default     = 12
+  description = "The number of most-recent AMIs for which to grant launch permission (e.g. \"3\").  If this variable is set to three, for example, then accounts will be granted permission to launch the three most recent AMIs (or all most recent AMIs, if there are only one or two of them in existence)."
+  type        = number
 }
